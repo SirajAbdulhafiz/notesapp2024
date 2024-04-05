29909630446e1f5117cea3184d1cf877382567bf
SirajAbdulhafiz <mansor.atta@gmail.com>
2024-04-05 13:35:56 +0300

    change them and create note_view and app bar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,35 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/notes_view_body.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          appBar: AppBar(
-            //title: const Text('Note App', style:  TextStyle(fontSize: 24),),
-            actions: [
-             Row(
-
-               children: [
-                 Text('Note App', style:  TextStyle(fontSize: 24),),
-                 Container(),
-
-                 Container(
-                   decoration: BoxDecoration(
-                       borderRadius: BorderRadius.circular(16),
-                       color: Colors.white12
-                   ),
-                   height: 60,
-                   width: 60,
-                   child: const Icon(
-                       Icons.search
-                   ),
-                 ),
-               ],
-             )
-            ],
-          ),
+    return const Scaffold(
+         body: NotesViewBody(),
     );
   }
 }
+
+
